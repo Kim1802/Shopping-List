1771b580d0fc4b8ee2ac2ea6c57473b3378ebd1b
Kim <baokim1802@users.noreply.github.com>
2018-12-14 18:48:56 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+var butt = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var items = document.getElementsByTagName("li");
+var delButtons = document.getElementsByClassName("delButton");
+
+function inputCheck() {
+	if (input.value.length > 0) {
+		var li = document.createElement("li");
+		li.appendChild(document.createTextNode(input.value));
+		ul.appendChild(li);
+		input.value = "";
+	}
+}
+
+function addElementByButton() {
+	inputCheck();
+	createDeleteButton();
+	deleteItem();
+}
+
+function addElementByEnter(event) {
+	if (event.keyCode === 13) {
+		inputCheck();
+		createDeleteButton();
+		deleteItem();
+	}
+}
+
+
+function lineThrough(item) {
+	if (item.target.tagName === "LI")
+		item.target.classList.toggle("done");
+
+}
+
+function createDeleteButton() {
+	for (var i = 0; i < items.length; i++) {
+		var newDeleteButton = document.createElement("button");
+		newDeleteButton.appendChild(document.createTextNode("Delete"));
+		newDeleteButton.classList.add("delButton");
+		items[i].appendChild(newDeleteButton);
+	}
+}
+
+function deleteItem() {
+	for (var i = 0; i < delButtons.length; i++) {
+		delButtons[i].addEventListener("click", function() {
+			this.parentNode.remove();
+		})
+	}
+}
+
+
+createDeleteButton();
+butt.addEventListener("click", addElementByButton);
+input.addEventListener("keypress", addElementByEnter);
+ul.addEventListener("click", lineThrough);
+deleteItem();
